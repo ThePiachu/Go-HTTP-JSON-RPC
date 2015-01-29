0771580d6fbac124170677f1aa71a3d0b0c97b9d
ThePiachu <ThePiachu@gmail.com>
2015-01-29 12:33:23 -0800

    Updated the repo a bit, moved the Bitcoin-specific code to https://github.com/ThePiachu/GoBitcoinAPI .

--- gaehttpjsonrpc/gaehttpjsonrpcClient.go
+++ gaehttpjsonrpc/gaehttpjsonrpcClient.go
@@ -1,97 +1,150 @@
 package gaehttpjsonrpc
 
+// Copyright 2011-2014 ThePiachu. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import (
-    "json"
-    "io/ioutil"
-    //"log"
-    "strings"
-    "os"
-    "appengine"
-    "appengine/urlfetch"
-    "http"
+	"appengine"
+	"appengine/urlfetch"
+	"encoding/json"
+	"github.com/ThePiachu/Go/Log"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
 
-func Call(c appengine.Context, address string, allowInvalidServerCertificate bool, method string, id interface{}, params []interface{})(map[string]interface{}, os.Error){
-    data, err := json.Marshal(map[string]interface{}{
-        "method": method,
-        "id":     id,
-        "params": params,
-    })
-    if err != nil {
-        c.Infof("Marshal: %v", err)
-    	return nil, err
-    }
-    
-    req, err:=http.NewRequest("POST", address, strings.NewReader(string(data)))
-    if err!=nil{
-        c.Infof("Request: %v", err)
-    	return nil, err
-    }
-    
-    tr := &urlfetch.Transport{Context: c}
-    tr.AllowInvalidServerCertificate=allowInvalidServerCertificate
-    
-    resp, err:=tr.RoundTrip(req)
-    if err != nil {
-        c.Infof("Post: %v", err)
-    	return nil, err
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        c.Infof("ReadAll: %v", err)
-    	return nil, err
-    }
-    result := make(map[string]interface{})
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        c.Infof("Unmarshal: %v", err)
-        c.Infof("%s", body)
-    	return nil, err
-    }
-    //log.Println(result)
-    return result, nil
+var TimeoutDuration time.Duration
+
+func init() {
+	TimeoutDuration, _ = time.ParseDuration("60s")
 }
 
-func CallWithBasicAuth(c appengine.Context, address string, username string, password string, allowInvalidServerCertificate bool, method string, id interface{}, params []interface{})(map[string]interface{}, os.Error){
-    data, err := json.Marshal(map[string]interface{}{
-        "method": method,
-        "id":     id,
-        "params": params,
-    })
-    if err != nil {
-        c.Infof("Marshal: %v", err)
-    	return nil, err
-    }
-    
-    req, err:=http.NewRequest("POST", address, strings.NewReader(string(data)))
-    if err!=nil{
-        c.Infof("Request: %v", err)
-    	return nil, err
-    }
-    req.SetBasicAuth(username, password)
-    
-    tr := &urlfetch.Transport{Context: c}
-    tr.AllowInvalidServerCertificate=allowInvalidServerCertificate
-    
-    resp, err:=tr.RoundTrip(req)
-    if err != nil {
-        c.Infof("Post: %v", err)
-    	return nil, err
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        c.Infof("ReadAll: %v", err)
-    	return nil, err
-    }
-    result := make(map[string]interface{})
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        c.Infof("Unmarshal: %v", err)
-        c.Infof("%s", body)
-    	return nil, err
-    }
-    //log.Println(result)
-    return result, nil
+func Call(c appengine.Context, address string, allowInvalidServerCertificate bool, method string, id interface{}, params []interface{}) (map[string]interface{}, error) {
+	Log.Debugf(c, "gaehttpjsonrpc Call - %v", method)
+	data, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"id":     id,
+		"params": params,
+	})
+	if err != nil {
+		Log.Infof(c, "Marshal: %v", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", address, strings.NewReader(string(data)))
+	if err != nil {
+		Log.Infof(c, "Request: %v", err)
+		return nil, err
+	}
+
+	tr := &urlfetch.Transport{Context: c, Deadline: TimeoutDuration, AllowInvalidServerCertificate: allowInvalidServerCertificate}
+
+	resp, err := tr.RoundTrip(req)
+	if err != nil {
+		Log.Infof(c, "Post: %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		Log.Infof(c, "ReadAll: %v", err)
+		return nil, err
+	}
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		Log.Infof(c, "Unmarshal: %v", err)
+		Log.Infof(c, "%s", body)
+		return nil, err
+	}
+	//log.Println(result)
+	return result, nil
+}
+
+func CallWithBasicAuth(c appengine.Context, address string, username string, password string, allowInvalidServerCertificate bool, method string, id interface{}, params []interface{}) (map[string]interface{}, error) {
+	Log.Debugf(c, "gaehttpjsonrpc CallWithBasicAuth - %v", method)
+	data, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"id":     id,
+		"params": params,
+	})
+	if err != nil {
+		Log.Infof(c, "Marshal: %v", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", address, strings.NewReader(string(data)))
+	if err != nil {
+		Log.Infof(c, "Request: %v", err)
+		return nil, err
+	}
+	req.SetBasicAuth(username, password)
+
+	tr := &urlfetch.Transport{Context: c, Deadline: TimeoutDuration, AllowInvalidServerCertificate: allowInvalidServerCertificate}
+
+	resp, err := tr.RoundTrip(req)
+	if err != nil {
+		Log.Infof(c, "Post: %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		Log.Infof(c, "ReadAll: %v", err)
+		return nil, err
+	}
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		Log.Infof(c, "Unmarshal: %v", err)
+		Log.Infof(c, "%s", body)
+		return nil, err
+	}
+	//log.Println(result)
+	return result, nil
+}
+
+func CallWithBasicAuthSingleParam(c appengine.Context, address string, username string, password string, allowInvalidServerCertificate bool, method string, id interface{}, params interface{}) (map[string]interface{}, error) {
+	Log.Debugf(c, "gaehttpjsonrpc CallWithBasicAuthSingleParam - %v", method)
+	data, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"id":     id,
+		"params": params,
+	})
+	if err != nil {
+		Log.Infof(c, "Marshal: %v", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", address, strings.NewReader(string(data)))
+	if err != nil {
+		Log.Infof(c, "Request: %v", err)
+		return nil, err
+	}
+	req.SetBasicAuth(username, password)
+
+	tr := &urlfetch.Transport{Context: c, Deadline: TimeoutDuration, AllowInvalidServerCertificate: allowInvalidServerCertificate}
+
+	resp, err := tr.RoundTrip(req)
+	if err != nil {
+		Log.Infof(c, "Post: %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		Log.Infof(c, "ReadAll: %v", err)
+		return nil, err
+	}
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		Log.Infof(c, "Unmarshal: %v", err)
+		Log.Infof(c, "%s", body)
+		return nil, err
+	}
+	//log.Println(result)
+	return result, nil
 }
--- gaehttpjsonrpc/gaehttpjsonrpcServer.go
+++ gaehttpjsonrpc/gaehttpjsonrpcServer.go
@@ -1,114 +1,123 @@
 package gaehttpjsonrpc
 
-import(
-	"http"
-	"io/ioutil"
-	"json"
+// Copyright 2011-2014 ThePiachu. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import (
 	"appengine"
+	"encoding/json"
+	"github.com/ThePiachu/Go/Log"
+	"io/ioutil"
+	"net/http"
 )
+
+func init() {
+	mainMux.m = map[string]func(http.ResponseWriter, *http.Request, map[string]interface{}) map[string]interface{}{}
+}
 
 //multiplexer that keeps track of every function to be called on specific rpc call
 type ServeMux struct {
-		m map[string]func(*http.Request, map[string]interface{})(map[string]interface{})
-		defaultFunction func(http.ResponseWriter, *http.Request)
+	m               map[string]func(http.ResponseWriter, *http.Request, map[string]interface{}) map[string]interface{}
+	defaultFunction func(http.ResponseWriter, *http.Request)
 }
 
 //an instance of the multiplexer
 var mainMux ServeMux
 
 //a function to register functions to be called for specific rpc calls
-func HandleFunc(pattern string, handler func(*http.Request, map[string]interface{})(map[string]interface{})){
-	mainMux.m[pattern]=handler
+func HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request, map[string]interface{}) map[string]interface{}) {
+	mainMux.m[pattern] = handler
 }
 
 //a function to be called if the request is not a HTTP JSON RPC call
-func SetDefaultFunc(def func(http.ResponseWriter, *http.Request)){
-	mainMux.defaultFunction=def
+func SetDefaultFunc(def func(http.ResponseWriter, *http.Request)) {
+	mainMux.defaultFunction = def
 }
+
 //this is the funciton that should be called in order to answer an rpc call
 //should be registered like "http.HandleFunc("/", httpjsonrpc.Handle)"
-func Handle(w http.ResponseWriter, r *http.Request){
+func Handle(w http.ResponseWriter, r *http.Request) {
 	//read the body of the request
-	c:=appengine.NewContext(r)
-    c.Debugf("HTTP JSON RPC Handle - Request - %v", r)
-    
-    //JSON RPC commands should be POSTs
-    if r.Method!="POST"{
-    	if mainMux.defaultFunction!=nil{
-    		c.Debugf("HTTP JSON RPC Handle - Method!=\"POST\"")
-    		mainMux.defaultFunction(w, r)
-    		return
-    	} else {
-    		c.Warningf("HTTP JSON RPC Handle - Method!=\"POST\"")
-    		return
-    	}
-    }
-    
-    //We must check if there is Request Body to read
-    if r.Body==nil{
-    	if mainMux.defaultFunction!=nil{
-    		c.Debugf("HTTP JSON RPC Handle - Request body is nil")
-    		mainMux.defaultFunction(w, r)
-    		return
-    	} else {
-    		c.Warningf("HTTP JSON RPC Handle - Request body is nil")
-    		return
-    	}
-    }
-	
-    body, err:= ioutil.ReadAll(r.Body)
-    c.Debugf("Body - %s", body)
-    if err!=nil{
-       	c.Errorf("HTTP JSON RPC Handle - ioutil.ReadAll: %v", err)
-        return
-    }
-    request := make(map[string]interface{})
-    //unmarshal the request
-    err = json.Unmarshal(body, &request)
-    if err != nil {
-        c.Warningf("HTTP JSON RPC Handle - json.Unmarshal: %v", err)
-    	return
-    }
-    //log.Println(request["method"])
-    
-    //get the corresponding function
-    function, ok:=mainMux.m[request["method"].(string)]
-    
-    if ok{//if the function exists, itis called
-    	response:=function(r, request)
-    	//response from the program is encoded
-    	data, err := json.Marshal(response)
-    	if err!=nil{
-	        c.Errorf("HTTP JSON RPC Handle - json.Marshal: %v", err)
-	        return
-    	}
-    	//result is printed to the output
-    	w.Write(data)
-    } else {//if the function does not exist
-    	c.Warningf("HTTP JSON RPC Handle - No function to call for", request["method"])
-    	/*
-    	//if you don't want to send an error, send something else:
-    	data, err := json.Marshal(map[string]interface{}{
-        	"result": "OK!",
-        	"error": nil,
-        	"id": request["id"],
-    	})*/
-    	//an error json is created
-    	data, err := json.Marshal(map[string]interface{}{
-        	"result": nil,
-        	"error": map[string]interface{}{
-        		"code": -32601,
-        		"message": "Method not found",
-        		"data": "The called method was not found on the server",
-        	
-        	},
-        	"id": request["id"],
-    	})
-    	if err!=nil{
-	        c.Errorf("HTTP JSON RPC Handle - json.Marshal: %v", err)
-	        return
-    	}
+	c := appengine.NewContext(r)
+	Log.Debugf(c, "HTTP JSON RPC Handle - Request - %v", r)
+
+	//JSON RPC commands should be POSTs
+	if r.Method != "POST" {
+		if mainMux.defaultFunction != nil {
+			Log.Debugf(c, "HTTP JSON RPC Handle - Method!=\"POST\"")
+			mainMux.defaultFunction(w, r)
+			return
+		} else {
+			Log.Warningf(c, "HTTP JSON RPC Handle - Method!=\"POST\"")
+			return
+		}
+	}
+
+	//We must check if there is Request Body to read
+	if r.Body == nil {
+		if mainMux.defaultFunction != nil {
+			Log.Debugf(c, "HTTP JSON RPC Handle - Request body is nil")
+			mainMux.defaultFunction(w, r)
+			return
+		} else {
+			Log.Warningf(c, "HTTP JSON RPC Handle - Request body is nil")
+			return
+		}
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	Log.Debugf(c, "Body - %s", body)
+	if err != nil {
+		Log.Errorf(c, "ioutil.ReadAll: %v", err)
+		return
+	}
+	request := make(map[string]interface{})
+	//unmarshal the request
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		Log.Warningf(c, "HTTP JSON RPC Handle - json.Unmarshal: %v", err)
+		return
+	}
+	//log.Println(request["method"])
+
+	//get the corresponding function
+	function, ok := mainMux.m[request["method"].(string)]
+
+	if ok { //if the function exists, it is called
+		response := function(w, r, request)
+		//response from the program is encoded
+		data, err := json.Marshal(response)
+		if err != nil {
+			Log.Errorf(c, "HTTP JSON RPC Handle - json.Marshal: %v", err)
+			return
+		}
+		//result is printed to the output
+		w.Write(data)
+	} else { //if the function does not exist
+		Log.Warningf(c, "HTTP JSON RPC Handle - No function to call for", request["method"])
+		/*
+		   	//if you don't want to send an error, send something else:
+		   	data, err := json.Marshal(map[string]interface{}{
+		       	"result": "OK!",
+		       	"error": nil,
+		       	"id": request["id"],
+		   	})*/
+		//an error json is created
+		data, err := json.Marshal(map[string]interface{}{
+			"result": nil,
+			"error": map[string]interface{}{
+				"code":    -32601,
+				"message": "Method not found",
+				"data":    "The called method was not found on the server",
+			},
+			"id": request["id"],
+		})
+		if err != nil {
+			Log.Errorf(c, "HTTP JSON RPC Handle - json.Marshal: %v", err)
+			return
+		}
 		//it is printed
-    	w.Write(data)
-    }
+		w.Write(data)
+	}
 }
