bd6312cdef189ec2fe79bac7042aedcce0a6b84e
Timothy Jones <timothy.l.jones@gmail.com>
2015-08-27 14:36:05 +1000

    Corrected imports to match current release; ran gofmt

--- httpjsonrpc/test.go
+++ httpjsonrpc/test.go
@@ -5,45 +5,44 @@ package httpjsonrpc
 // license that can be found in the LICENSE file.
 
 import (
-    "json"
-    "io/ioutil"
-    "log"
-    "http"
-    "strings"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
-
-func Main(){
-	res, err:=Call("http://user:pass@127.0.0.1:8332", "getinfo", 1, []interface{}{})
-	if err!=nil{
-        log.Fatalf("Err: %v", err)
+func Main() {
+	res, err := Call("http://user:pass@127.0.0.1:8332", "getinfo", 1, []interface{}{})
+	if err != nil {
+		log.Fatalf("Err: %v", err)
 	}
 	log.Println(res)
 }
 
 func tmp() {
-    data, err := json.Marshal(map[string]interface{}{
-        "method": "getinfo",
-        "id":     1,
-        "params": []interface{}{},
-    })
-    if err != nil {
-        log.Fatalf("Marshal: %v", err)
-    }
-    resp, err := http.Post("http://user:pass@127.0.0.1:8332",
-        "application/json", strings.NewReader(string(data)))
-    if err != nil {
-        log.Fatalf("Post: %v", err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalf("ReadAll: %v", err)
-    }
-    result := make(map[string]interface{})
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        log.Fatalf("Unmarshal: %v", err)
-    }
-    log.Println(result)
+	data, err := json.Marshal(map[string]interface{}{
+		"method": "getinfo",
+		"id":     1,
+		"params": []interface{}{},
+	})
+	if err != nil {
+		log.Fatalf("Marshal: %v", err)
+	}
+	resp, err := http.Post("http://user:pass@127.0.0.1:8332",
+		"application/json", strings.NewReader(string(data)))
+	if err != nil {
+		log.Fatalf("Post: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("ReadAll: %v", err)
+	}
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	log.Println(result)
 }
